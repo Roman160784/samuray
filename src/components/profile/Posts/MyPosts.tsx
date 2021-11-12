28ceefb01185fdb0c2d00d651d5637f93421962c
Roman160784 <matveyenkaraman@gmail.com>
2021-11-12 11:20:07 +0300

    12.11.2021

--- src/components/profile/Posts/MyPosts.tsx
+++ src/components/profile/Posts/MyPosts.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import p from './MyPosts.module.css'
 import MyPost from './post/MyPost'
 
+
 function MyPosts() {
+ 
   return (
       <div>
         my posts
         <div>
-         <MyPost />
+         <MyPost message='Hi'likesCount='0'/>
+         <MyPost  message='Hey' likesCount='23'/>
           <textarea></textarea>
           <button>Add Post</button>
         </div>
--- src/components/profile/Posts/post/MyPost.tsx
+++ src/components/profile/Posts/post/MyPost.tsx
@@ -1,13 +1,23 @@
+import { type } from 'os';
 import React from 'react';
 import p from './MyPost.module.css'
 
-function MyPost() {
+
+type PropsType = {
+  message: string
+  likesCount: string
+}
+
+function MyPost (props: PropsType) {
   return (
     <div>
       <div className={p.posts}>
         <div className={p.iteam}>
           <img src="https://cs16planet.ru/steam-avatars/images/avatar3219.jpg" />
-          post1
+          {props.message} 
+          <div>
+          <span>Like</span>{props.likesCount}
+          </div>
         </div>
       </div>
     </div>
