475d944d64da8b22cee090f103717f929b06d93e
Roman160784 <matveyenkaraman@gmail.com>
2022-03-17 10:31:06 +0300

    refactor

--- src/redux/Profile-reducer.ts
+++ src/redux/Profile-reducer.ts
@@ -14,29 +14,6 @@ let initialState: ProfilePageType = {
     newPostText: "it-Kamasutra",
     profile: null,
     status: '',
-
-        // {
-        //     aboutMe: "я круто чувак 1001%",
-        //     contacts: {
-        //         facebook: "facebook.com",
-        //         website: null,
-        //         vk: "vk.com/dimych",
-        //         twitter: "https://twitter.com/@sdf",
-        //         instagram: "instagra.com/sds",
-        //         youtube: null,
-        //         github: "github.com",
-        //         mainLink: null,
-        //     },
-        //     lookingForAJob: true,
-        //     lookingForAJobDescription: "не ищу, а дурачусь",
-        //     fullName: "samurai dimych",
-        //     userId: 2,
-        //     photos: {
-        //         small: "https://social-network.samuraijs.com/activecontent/images/users/2/user-small.jpg?v=0",
-        //         large: "https://social-network.samuraijs.com/activecontent/images/users/2/user.jpg?v=0",
-        //       }
-        // }
-
 }
 const initState = {} as ProfilePageType 
 export const profileReducer = (state: ProfilePageType = initialState, action: AppActionType): ProfilePageType => {
