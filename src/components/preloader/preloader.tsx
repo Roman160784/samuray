115476ad7303214bea31f9049db84c5c8d38379c
Roman160784 <matveyenkaraman@gmail.com>
2022-03-21 16:18:04 +0300

    add handmade preloader

--- src/components/preloader/preloader.tsx
+++ src/components/preloader/preloader.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import preloader from '../../assets/gif/preloader.gif'
+import s from './preloader.module.css'
 
 export const Preloader = () => {
-    return <div> <img src={preloader}/> </div>
+    // return <div> <img src={preloader}/> </div>
+    return (
+        <div className={s.center}>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+      <div className={s.wave}></div>
+    </div>
+        )
 }
